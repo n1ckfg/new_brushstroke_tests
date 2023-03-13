751cdc8e8cc52e13aff6df84ae542d30af4f3b28
Nick Fox-Gieg <nick@fox-gieg.com>
2023-03-13 02:37:58 -0400

    Update brushstrokes_from_mesh.py

--- brushstrokes_from_mesh.py
+++ brushstrokes_from_mesh.py
@@ -12,8 +12,9 @@ mesh = loadMesh(inputPath)
 
 bounds = getBounds(mesh)
 
-max_distance = bounds * 0.1
-max_points = 100
+max_distance = bounds * 0.02
+max_points = 300
+min_points = 5
 skip_points = 4
 
 sharp = mesh.face_adjacency_angles > np.radians(90)
@@ -42,15 +43,18 @@ for i in range(1, len(lines), skip_points):
         makeNewStroke = True
 
     if (makeNewStroke == True):
-        for la_p in la_s.points:
-            la_p.co = [la_p.co[0], la_p.co[2], la_p.co[1]]
+        if (len(la_s.points) > min_points):
+            for la_p in la_s.points:
+                la_p.co = [la_p.co[0], la_p.co[2], la_p.co[1]]
 
-        la.layers[0].frames[0].strokes.append(la_s)
+            la.layers[0].frames[0].strokes.append(la_s)
+        
         la_s = latk.LatkStroke()
         la_p = latk.LatkPoint(co=new_point)
         la_s.points.append(la_p)
 
 if (len(la.layers[0].frames[0].strokes) > 0):
+    la.normalize()
     la.write("output.latk")
 else:
     print("No strokes generated.")
