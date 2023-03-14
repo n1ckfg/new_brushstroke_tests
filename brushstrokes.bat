b9f01309278e6baedbfb2043deaa1d54ec085784
Nick Fox-Gieg <nick@fox-gieg.com>
2023-03-14 08:07:47 -0400

    Create brushstrokes_from_mesh_2.py
    shell
    
    test

--- brushstrokes.bat
+++ brushstrokes.bat
@@ -1,6 +1,6 @@
 @echo off
 
 cd %~dp0
-python brushstrokes_from_mesh.py -- %1
+python brushstrokes_from_mesh_2.py -- %1
 
 @pause
---
+++ brushstrokes_from_mesh_2.py
@@ -0,0 +1,51 @@
+import trimesh
+import numpy as np
+import latk
+from scipy.spatial.distance import cdist
+from common import *
+
+argv = sys.argv[sys.argv.index("--") + 1:] # get all args after "--"
+inputPath = argv[0]
+
+la = latk.Latk(init=True)
+
+mesh = trimesh.load(inputPath)
+bounds = getBounds(mesh)
+searchRadius = bounds * 0.02
+print("Search radius: " + str(searchRadius))
+
+def group_points_into_strokes(points, radius):
+    strokes = []
+    unassigned_points = set(range(len(points)))
+
+    while len(unassigned_points) > 0:
+        stroke = [next(iter(unassigned_points))]
+        unassigned_points.remove(stroke[0])
+
+        for i in range(len(points)):
+            if i in unassigned_points and cdist([points[i]], [points[stroke[-1]]])[0][0] < radius:
+                stroke.append(i)
+                unassigned_points.remove(i)
+
+        if (len(stroke) > 1):
+        	strokes.append(stroke)
+        print("Found " + str(len(strokes)) + " strokes, " + str(len(unassigned_points)) + " points remaining.")
+    return strokes
+
+strokes = group_points_into_strokes(mesh.vertices, searchRadius)
+
+for stroke in strokes:
+	la_stroke = latk.LatkStroke()
+	for index in stroke:
+		vert = mesh.vertices[index]
+		la_point = latk.LatkPoint(co=(vert[0], vert[2], vert[1]))
+		la_stroke.points.append(la_point)
+	la.layers[0].frames[0].strokes.append(la_stroke)
+
+if (len(la.layers[0].frames[0].strokes) > 0):
+    la.normalize()
+    la.write("output.latk")
+else:
+    print("No strokes generated.")
+
+
